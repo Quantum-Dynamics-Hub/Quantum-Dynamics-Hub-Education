068c2307da50d470244a1aa8b803869a4f1d7d5f
Anthony Dominic <anthony.dominiciii@colorado.edu>
2021-06-18 21:14:54 -0400

    adding work for the first week

--- course_work/ajdominic/Day_4/2_nbra_workflows/5_step4/1_initialize_data/README.md
+++
@@ -1,7 +0,0 @@
-[Go back to TOC](../../../../README.md)
-
-**Required files**
-
-*  res_ks_step2_compressed
-*  res_sd_step3_compressed
-
--- course_work/ajdominic/Day_4/exercises/1_qd_build/insert_dummies.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from scm.plams import Molecule
-from CAT.recipes import replace_surface
-mol = Molecule('cspbbr3_4.2nm.xyz')
-mol_new = replace_surface(mol, symbol='Br', symbol_new='Cl', f=0.8, mode='uniform', displacement_factor=0.7)
-mol_new.write('cspbbr3_4.2nm_80Cl.xyz')
